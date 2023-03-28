729ea14a4fa53881ddbf50df12a4b751d3c0ac81
Srini K <srinikdevops0131@gmail.com>
2023-03-27 22:01:37 -0700

    updating ./tf-module-elasticache

--- main.tf
+++ main.tf
@@ -22,3 +22,13 @@ resource "aws_elasticache_subnet_group" "main" {
     { Name = "${var.env}-subnet-group" }
   )
 }
+
+#output "redis" {
+#  value = aws_elasticache_cluster.elasticache
+#}
+#
+#resource "aws_ssm_parameter" "elasticache_endpoint" {
+#  name  = "${var.env}.elasticache.endpoint"
+#  type  = "String"
+#  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
+#}
